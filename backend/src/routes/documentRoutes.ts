daa040c35cde52c72daf7988c50e0807123460f2
miltonpalmeira <lpsantana_@hotmail.com>
2024-10-07 11:28:25 -0300

    refactor(documentRoute): documentRoutes changed - interface custom request created

--- backend/src/routes/documentRoutes.ts
+++ backend/src/routes/documentRoutes.ts
@@ -1,17 +1,19 @@
-import { Router } from "express";
+import { Router, Response, NextFunction } from "express";
 import prisma from "../../prisma/PrismaClient";
-import { authenticateToken } from "../middlewares/autheticateToken";
+import { authenticateToken } from "../middlewares/authenticateToken";
+import { CustomRequest } from "../interfaces/CustomRequest";
 
 const router = Router();
 
-router.get("/", authenticateToken, async (req, res) => {
+router.get("/", authenticateToken, async (req: CustomRequest, res: Response): Promise<void> => {
   try {
     const documents = await prisma.document.findMany({
-      where: { userId: (req as any).user.id },
+      where: { userId: req.user?.id },
     });
-    return res.json(documents);
+    res.json(documents);
   } catch (error) {
-    return res.status(500).json({ message: "Internal Server Error" });
+    console.log(error);
+    res.status(500).json({ message: "Internal Server Error" });
   }
 });
 
