89654addf56fae6d117fa438445d0955c6573c7f
miltonpalmeira <lpsantana_@hotmail.com>
2024-10-07 11:27:17 -0300

    refactor(middleware): change the name of the authenticateToken - req type e return type added

--- backend/src/middlewares/authenticateToken.ts
+++ backend/src/middlewares/authenticateToken.ts
@@ -1,27 +1,34 @@
-import { Request, Response, NextFunction } from "express";
+import { Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
+import { CustomRequest } from "../interfaces/CustomRequest"; 
 
 dotenv.config();
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
 export const authenticateToken = (
-  req: Request,
+  req: CustomRequest,
   res: Response,
   next: NextFunction
-) => {
+): void => {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
 
-  if (!token)
-    return res
+  if (!token) {
+    res
       .status(401)
       .json({ message: "Access Denied: No token provided" });
-  jwt.verify(token, JWT_SECRET!, (err, user) => {
-    if (err) return res.status(403).json({ message: "Invalid Token" });
+    return
+  }
 
-    (req as any).user = user;
+  jwt.verify(token, JWT_SECRET!, (err, user) => {
+    if (err) { 
+      res.status(403).json({ message: "Invalid Token" });
+      return;
+    }
+
+    req.user = user;
     next();
   });
 };
