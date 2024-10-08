81a18bdf16f31765c93976f957605e72d41a2ee5
miltonpalmeira <lpsantana_@hotmail.com>
2024-10-08 11:11:59 -0300

    refactor(controllers): document routes were refactored to work with controllers

--- backend/src/routes/documentRoutes.ts
+++ backend/src/routes/documentRoutes.ts
@@ -1,20 +1,10 @@
-import { Router, Response, NextFunction } from "express";
-import prisma from "../../prisma/PrismaClient.js";
+import { Router } from "express";
 import { authenticateToken } from "../middlewares/authenticateToken.js";
-import { CustomRequest } from "../interfaces/CustomRequest.js";
+import { createDocument, getDocuments } from "../controllers/documentController.js";
 
 const router = Router();
 
-router.get("/", authenticateToken, async (req: CustomRequest, res: Response): Promise<void> => {
-  try {
-    const documents = await prisma.document.findMany({
-      where: { userId: req.user?.id },
-    });
-    res.json(documents);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "Internal Server Error" });
-  }
-});
+router.post("/", authenticateToken, createDocument);
+router.get("/", authenticateToken, getDocuments);
 
 export default router;
