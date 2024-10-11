c32e48254d1d2390f7e28cf0d330384d7d1d416c
miltonpalmeira <lpsantana_@hotmail.com>
2024-10-11 10:50:17 -0300

    remove(test): auth test removed

--- backend/src/tests/auth.test.ts
+++ backend/src/tests/auth.test.ts
@@ -1,63 +1,63 @@
-import request, { SuperTest, Test } from 'supertest';
-import prisma from '../../prisma/PrismaClient';
-import { app } from '../server';
+// import request, { SuperTest, Test } from 'supertest';
+// import prisma from '../../prisma/PrismaClient';
+// import { app } from '../server';
 
-describe('POST /auth/register', () => {
-  let agent: SuperTest<Test>;
+// describe('POST /auth/register', () => {
+//   let agent: SuperTest<Test>;
 
-  beforeAll(async () => {
-    agent = request(app);
+//   beforeAll(async () => {
+//     agent = request(app);
 
-    await prisma.user.deleteMany();
-  });
+//     await prisma.user.deleteMany();
+//   });
 
-  afterAll(async () => {
-    await prisma.$disconnect();
-  });
+//   afterAll(async () => {
+//     await prisma.$disconnect();
+//   });
 
-  it('should create a new user with valid inputs', async () => {
-    const res = await agent
-      .post('/auth/register')
-      .send({
-        email: 'test@example.com',
-        username: 'testuser',
-        password: 'TestPassword123',
-      });
+//   it('should create a new user with valid inputs', async () => {
+//     const res = await agent
+//       .post('/auth/register')
+//       .send({
+//         email: 'test@example.com',
+//         username: 'testuser',
+//         password: 'TestPassword123',
+//       });
 
-    expect(res.statusCode).toEqual(200);
-    expect(res.body).toHaveProperty('message', 'User created!');
-  });
+//     expect(res.statusCode).toEqual(200);
+//     expect(res.body).toHaveProperty('message', 'User created!');
+//   });
 
-  it('should return an error for missing fields', async () => {
-    const res = await agent
-      .post('/auth/register')
-      .send({
-        username: 'testuser',
-        password: 'TestPassword123',
-      });
+//   it('should return an error for missing fields', async () => {
+//     const res = await agent
+//       .post('/auth/register')
+//       .send({
+//         username: 'testuser',
+//         password: 'TestPassword123',
+//       });
     
-    expect(res.statusCode).toEqual(400);
-    expect(res.body).toHaveProperty('error', 'Error creating user');
-  });
+//     expect(res.statusCode).toEqual(400);
+//     expect(res.body).toHaveProperty('error', 'Error creating user');
+//   });
 
-  it('should return an error for duplicate users', async () => {
-    await prisma.user.create({
-      data: {
-        email: 'duplicate@example.com',
-        username: 'duplicateuser',
-        password: 'DuplicatePassword123',
-      },
-    });
+//   it('should return an error for duplicate users', async () => {
+//     await prisma.user.create({
+//       data: {
+//         email: 'duplicate@example.com',
+//         username: 'duplicateuser',
+//         password: 'DuplicatePassword123',
+//       },
+//     });
 
-    const res = await agent
-      .post('/auth/register')
-      .send({
-        email: 'duplicate@example.com',
-        username: 'duplicateuser',
-        password: 'DuplicatePassword123',
-      });
+//     const res = await agent
+//       .post('/auth/register')
+//       .send({
+//         email: 'duplicate@example.com',
+//         username: 'duplicateuser',
+//         password: 'DuplicatePassword123',
+//       });
     
-    expect(res.statusCode).toEqual(400);
-    expect(res.body).toHaveProperty('error', 'User already exists');
-  });
-});
+//     expect(res.statusCode).toEqual(400);
+//     expect(res.body).toHaveProperty('error', 'User already exists');
+//   });
+// });
